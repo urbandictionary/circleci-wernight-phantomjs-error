57709139ed5f79fd1f9f70f9e0007b30e44be0f5
Aaron <apeckham@gmail.com>
2016-05-24 16:54:04 -0700

    Delete unused script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log('Loading a web page');
-var page = require('webpage').create();
-var url = 'http://phantomjs.org/';
-page.open(url, function (status) {
-  console.log('Page is loaded!');
-  phantom.exit();
-});
